a2b3d1185f2ac4904abe3985ef4788ddc2260fba
Wilson Lee <kourge@gmail.com>
2016-03-25 02:50:00 -0700

    Add tests

---
+++ spec/index.spec.ts
@@ -0,0 +1,19 @@
+import {expect} from 'chai';
+
+import {toFailable, isSuccess, isFailure} from '../src';
+
+describe('toFailable', () => {
+  it('produces a Success<T> when the function returns without throwing', () => {
+    const result = toFailable(() => "foo");
+
+    expect(isSuccess(result)).to.be.true;
+  });
+
+  it('produces a Failure when the the function throws', () => {
+    const result = toFailable((): string => {
+      throw new Error();
+    });
+
+    expect(isFailure(result)).to.be.true;
+  });
+});
